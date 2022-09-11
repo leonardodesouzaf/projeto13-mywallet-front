0d1d66375a3f79d6cb40947a3d8e93aec8250282
Leonardo de Souza Ferreira <leonardosouzaf@icloud.com>
2022-09-11 19:59:21 -0300

    linking buttons of home display

--- src/Components/HomeDisplay/HomeDisplay.js
+++ src/Components/HomeDisplay/HomeDisplay.js
@@ -19,20 +19,23 @@ export default function HomeDisplay() {
         });
         function renderTransactions(answer) {
             let transactionsList = answer.data;
+            console.log(transactionsList);
             if (transactionsList.length === 0) {
                 setTransactionsContent(
-                    <NonTransactionsText>
-                        Não há registros de entrada ou saída
-                    </NonTransactionsText>
+                    <NonTransactionsDiv>
+                        <NonTransactionsText>
+                            Não há registros de entrada ou saída
+                        </NonTransactionsText>
+                    </NonTransactionsDiv>
                 );
             } else {
                 setTransactionsContent(
-                    <>
+                    <TransactionsDiv>
                         {transactionsList.map((transaction,index) => {
                             totalCounter += transaction.value;
                             return(<Transaction key={index} date={transaction.date} description={transaction.description} value={transaction.value} type={transaction.type} />);
                         })}
-                    </>
+                    </TransactionsDiv>
                 )
             }
         }
@@ -40,11 +43,55 @@ export default function HomeDisplay() {
     return (
         <Content>
             <TitleDiv>
-                <p>Olá, Fulano</p>
+                <p>Olá, {tasks.name}</p>
+                <Link to="/">
+                    <ion-icon name="log-out-outline"></ion-icon>
+                </Link>
             </TitleDiv>
+            {transactionsContent}
+            <ButtonsDiv>
+                <ButtonTransaction>
+                    <ion-icon name="add-circle-outline"></ion-icon>
+                    <ButtonText>Nova entrada</ButtonText>
+                </ButtonTransaction>
+                <ButtonTransaction>
+                    <ion-icon name="remove-circle-outline"></ion-icon>
+                    <ButtonText>Nova saída</ButtonText>
+                </ButtonTransaction>
+            </ButtonsDiv>
         </Content>
     );
 }
+
+const ButtonText = styled.div`
+    font-size: 17px;
+    width: 40%;
+`;
+
+const ButtonTransaction = styled.div`
+    background-color: #A328D6;
+    border-radius: 5px;
+    height: 114px;
+    width: 48%;
+    box-sizing: border-box;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 22px;
+    line-height: 20px;
+    color: #FFFFFF;
+`;
+
+const ButtonsDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-top: 13px;
+`;
 
 const TitleDiv = styled.div`
     width: 100%;
@@ -56,6 +103,8 @@ const TitleDiv = styled.div`
     font-size: 26px;
     line-height: 31px;
     color: #FFFFFF;
+    box-sizing: border-box;
+    margin-bottom: 22px;
 `;
 
 const Content = styled.div`
@@ -76,4 +125,27 @@ const NonTransactionsText = styled.div`
     line-height: 23px;
     text-align: center;
     color: #868686;
+    width: 60%;
+`;
+
+const NonTransactionsDiv = styled.div`
+    background: #FFFFFF;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 446px;
+`;
+
+const TransactionsDiv = styled.div`
+    background: #FFFFFF;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 86.9%;
+    height: 446px;
+    box-sizing: border-box;
+    padding: 23px 10px 10px 10px;
 `;
